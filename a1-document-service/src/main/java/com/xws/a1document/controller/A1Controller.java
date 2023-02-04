9b5de1459ac2849d0c31d3a56357249f37049d52
Djordje Jovanovic <djordjejovanovic27@gmail.com>
2023-02-04 16:52:50 +0100

    Small changes in controller

--- a1-document-service/src/main/java/com/xws/a1document/controller/A1Controller.java
+++ a1-document-service/src/main/java/com/xws/a1document/controller/A1Controller.java
@@ -183,20 +183,16 @@ public class A1Controller {
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 	
-	@GetMapping(value = "getJson", produces = "application/json")
-    public ResponseEntity<String> getJsonMetadataById(@RequestParam(name = "id") String id) throws Exception {
-		String result = a1Service.getMetadataById(id, "JSON");
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentDispositionFormData("attachment", id + ".json");
-        return new ResponseEntity<>(result, headers, HttpStatus.OK);        
+	@GetMapping(value = "getJson")
+    public ResponseEntity<?> getJsonMetadataById(@RequestParam(name = "id") String id) throws Exception {
+		String result = a1Service.getMetadataById(id, "JSON");              
+        return new ResponseEntity<>(result, HttpStatus.OK);        
     }
 
-    @GetMapping(value = "getRdf", produces = "application/rdf+xml")
+    @GetMapping(value = "getRdf", produces = "application/xml")
     public ResponseEntity<String> getRdfMetadataById(@RequestParam(name = "id") String id) throws Exception {
-        String result = a1Service.getMetadataById(id, "RDF");
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentDispositionFormData("attachment", id + ".rdf");
-        return new ResponseEntity<>(result, headers, HttpStatus.OK);
+        String result = a1Service.getMetadataById(id, "RDF");        
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 	
 }
